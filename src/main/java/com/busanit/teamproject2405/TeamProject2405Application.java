b3d80243938c00c96b6da3af690ed029d719b56e
KimKay1 <kay.kimsy@gmail.com>
2024-05-10 15:42:42 +0900

    Initial commit

---
+++ src/main/java/com/busanit/teamproject2405/TeamProject2405Application.java
@@ -0,0 +1,13 @@
+package com.busanit.teamproject2405;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProject2405Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProject2405Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/busanit/teamproject2405/TeamProject2405ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busanit.teamproject2405;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProject2405ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
