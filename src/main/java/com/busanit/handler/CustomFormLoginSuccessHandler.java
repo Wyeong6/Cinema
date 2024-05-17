600f311064ddbc51c5cf0bde3650e816fe921630
KimKay1 <kay.kimsy@gmail.com>
2024-05-17 10:43:14 +0900

    [기능 변경] USER와 ADMIN 로그인 성공 페이지 분리

--- src/main/java/com/busanit/handler/CustomFormLoginSuccessHandler.java
+++ src/main/java/com/busanit/handler/CustomFormLoginSuccessHandler.java
@@ -6,22 +6,32 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
+import java.util.Set;
 
 @Log4j2
 public class CustomFormLoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        FormMemberDTO formMemberDTO = (FormMemberDTO) authentication.getPrincipal();
+//        FormMemberDTO formMemberDTO = (FormMemberDTO) authentication.getPrincipal();
+//
+//        // 소셜 로그인이 아닐 경우
+//        if(!formMemberDTO.isSocial()){
+//            log.info("일반 로그인");
+//            response.sendRedirect("/");
+//        }else {
+//            log.info("소설 로그인 (일반 로그인 불가)");
+//            response.sendRedirect("/");
+//        }
 
-        // 소셜 로그인이 아닐 경우
-        if(!formMemberDTO.isSocial()){
-            log.info("일반 로그인");
-            response.sendRedirect("/");
-        }else {
-            log.info("소설 로그인 (일반 로그인 불가)");
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+
+        if (roles.contains("ROLE_ADMIN")) {
+            response.sendRedirect("/admin/adminMain");
+        } else {
             response.sendRedirect("/");
         }
     }
