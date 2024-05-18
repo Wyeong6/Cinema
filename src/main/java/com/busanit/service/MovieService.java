485f6c10d6772f4f11c25ab4bd6658d89a424381
Wooyeong6 <dnduddl1381@gmail.com>
2024-05-19 01:15:15 +0900

    브런치복사

--- src/main/java/com/busanit/service/MovieService.java
+++ src/main/java/com/busanit/service/MovieService.java
@@ -28,7 +28,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
