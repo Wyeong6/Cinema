7d65b3015dcd7b037653eb915dc5c3761e8caacd
KimKay1 <kay.kimsy@gmail.com>
2024-05-20 16:41:36 +0900

    [기능 변경] snack page

--- src/main/java/com/busanit/service/SnackService.java
+++ src/main/java/com/busanit/service/SnackService.java
@@ -1,10 +1,41 @@
 package com.busanit.service;
 
+import com.busanit.domain.SnackDTO;
+import com.busanit.entity.Snack;
+import com.busanit.repository.SnackRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SnackService {
+    private final SnackRepository snackRepository;
 
+    // 리스트 불러오기
+    public Page<SnackDTO> getSnackList(Pageable pageable) {
+        Page<Snack> snackList = snackRepository.findAll(pageable);
+
+        // Page<Entity> -> Page<Dto> 변환
+        return snackList.map(entity -> {
+            return SnackDTO.builder()
+                    .snack_item_id(entity.getSnack_item_id())
+                    .snack_nm(entity.getSnack_nm())
+                    .snack_image(entity.getSnack_image())
+                    .snack_alt(entity.getSnack_alt())
+                    .snack_price(entity.getSnack_price())
+                    .snack_stock(entity.getSnack_stock())
+                    .snack_set(entity.getSnack_set())
+                    .snack_detail(entity.getSnack_detail())
+                    .regDate(entity.getRegDate())
+                    .updateDate(entity.getUpdateDate())
+                    .build();
+        });
+    }
+
+    // 스낵 저장(관리자 페이지)
+    public void saveSnack(Snack snack) {
+        snackRepository.save(snack);
+    }
 }
