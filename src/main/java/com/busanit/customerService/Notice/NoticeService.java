ea8ad194230febe5ce3eda85e1690af1e254d096
NohGiBong <nodarbong@gmail.com>
2024-05-22 01:02:29 +0900

    dd

--- src/main/java/com/busanit/customerService/Notice/NoticeService.java
+++ src/main/java/com/busanit/customerService/Notice/NoticeService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
@@ -46,12 +45,6 @@ public class NoticeService {
         model.addAttribute("pageNumbers", pageNumbers);
     }
 
-
-    public void incrementViewCount(com.busanit.customerService.Notice.Notice notice) {
-        notice.setViewCount(notice.getViewCount() + 1);
-        noticeRepository.save(notice);
-    }
-
     public Page<NoticeDTO> getNoticePaginated(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
         Page<Notice> noticePage = noticeRepository.findAllOrderedByPinnedAndId(pageable);
@@ -59,6 +52,14 @@ public class NoticeService {
 
         return noticePage.map(noticeMapper::toNoticeDTO);
     }
+
+
+    public void incrementViewCount(com.busanit.customerService.Notice.Notice notice) {
+        notice.setViewCount(notice.getViewCount() + 1);
+        noticeRepository.save(notice);
+    }
+
+
 
     public Notice getNoticeById(Long id) {
         return noticeRepository.findById(id).orElse(null);
