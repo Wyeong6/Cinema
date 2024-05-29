bb448782827cbed59754157e8ab8ce265f641dd3
qudgns8883 <qudgns8882@naver.com>
2024-05-29 16:37:50 +0900

    채팅 구현중

--- src/main/java/com/busanit/controller/ChatController.java
+++ src/main/java/com/busanit/controller/ChatController.java
@@ -1,15 +1,13 @@
 package com.busanit.controller;
 
 import com.busanit.domain.chat.MessageDTO;
-import com.busanit.entity.chat.Message;
 import com.busanit.service.ChatService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,12 +19,12 @@ public class ChatController {
     private final ChatService chatService;
     private final SimpMessagingTemplate messagingTemplate;
 
-    @GetMapping("/chat")
-    public String chat(Model model) {
+    @GetMapping("/chatUser")
+    public String chatUser(Model model) {
         if (chatService.isAuthenticated()) {
             String userEmail = chatService.getAuthenticatedUserEmail();
             model.addAttribute("userEmail", userEmail);
-            return "WebSocket"; // templates 폴더의 WebSocket.html 파일을 렌더링
+            return "client"; // templates 폴더의 client.html 파일을 렌더링
         } else {
             return "redirect:/login"; // 로그인 페이지로 리다이렉트
         }
@@ -49,8 +47,27 @@ public class ChatController {
         System.out.println("Creating chat room with title: " + messageDTO.getMessageTitle());
         chatService.getOrCreateChatRoom(messageDTO.getMessageTitle(), messageDTO.getSender());
 
+    }
+    @GetMapping("/chatAdmin")
+    public String chatAdmin(Model model) {
+
+            String adminEmail = chatService.getAuthenticatedUserEmail();
+            model.addAttribute("adminEmail", adminEmail);
+            return "service";
 
     }
 
+    @MessageMapping("/chat/admin")
+    public void sendAdminMessage(@Payload MessageDTO messageDTO) {
+
+        messagingTemplate.convertAndSendToUser(messageDTO.getRecipient(), "/queue/private", messageDTO);
+    }
+
+        //모든 구독자에게 메세지전송
+//    @MessageMapping("/chat/admin")
+//    @SendTo("/topic/messages") //브로드캐스트기능 , /topic/messages를 구독한 모든 사용자에게 메세지를 전송
+//    public MessageDTO broadcastMessage(@Payload MessageDTO messageDTO) {
+//        return messageDTO;
+//    }
 
 }
