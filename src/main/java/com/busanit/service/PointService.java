945a0e8c74ae7c6284b2d8fa277752c761864469
KimKay1 <kay.kimsy@gmail.com>
2024-06-03 11:45:18 +0900

    [새 기능] 마이페이지 멤버 등급 구현 (결제에도 멤버 등급 확인+저장 코드 넣을 예정)

--- src/main/java/com/busanit/service/PointService.java
+++ src/main/java/com/busanit/service/PointService.java
@@ -8,18 +8,29 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class PointService {
     private final PointRepository pointRepository;
 
+    // 포인트 적립
     public void savePoint(Point point) {
         pointRepository.save(point);
     }
 
+    // 포인트 내역
     public Slice<PointDTO> getPointInfo(Long member_id, Pageable pageable) {
         Slice<Point> pointList = pointRepository.findByMember_Id(member_id, pageable);
 
         return PointDTO.toDTOList(pointList);
+    }
+
+    // 최근 3개월간 영화관람 count
+    public long getPointMovieCount(Long memberId) {
+        LocalDateTime endDate = LocalDateTime.now();
+        LocalDateTime startDate = endDate.minusMonths(3);
+        return pointRepository.countByMovieMembership(memberId, startDate, endDate);
     }
 }
