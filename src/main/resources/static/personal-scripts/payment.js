d9c54295eda24533228644b64892c62a2fbe30a7
KimKay1 <kay.kimsy@gmail.com>
2024-07-04 15:26:09 +0900

    [기능 수정] sweetalert2 적용 [리팩토링]

--- src/main/resources/static/personal-scripts/payment.js
+++ src/main/resources/static/personal-scripts/payment.js
@@ -67,37 +67,37 @@ function requestPay() {
 
 
                     /* 테스트용 */
-                    $.ajax({
-                        type: "POST",
-                        url: "/payment/complete",
-                        data: $.param({
-                            "merchant_uid": rsp.merchant_uid,
-                            "imp_uid": rsp.imp_uid,
-                            "apply_num": rsp.apply_num,
-                            "buyer_email": rsp.buyer_email,
-                            "product_name": orderName,
-                            "product_idx": productIdx,
-                            "schedule_id": scheduleId,
-                            "product_type": reqIDX,
-                            "content1": content1,
-                            "content2": content2,
-                            "content3": content3,
-                            "content4": content4,
-                            "product_count": productCount,
-                            "amount": currentPrice,
-                            "plusPoint": plusPoint,
-                            "minusPoint": minusPoint
-                        }),
-                        success: function(response_complete) {
-                            localStorage.clear(); // 결제 완료 시 장바구니에 담긴 물품들 삭제
-                            let params = new URLSearchParams();
-                            params.append("imp_uid", response_complete.imp_uid);
-                            window.location.href = '/payment/paymentSuccessful?'+ params.toString(); // 결제가 완료된 후 리디렉션할 페이지
-                        },
-                        error: function() {
-                            alert("서버 통신에 실패했습니다.");
-                        }
-                    });
+                    // $.ajax({
+                    //     type: "POST",
+                    //     url: "/payment/complete",
+                    //     data: $.param({
+                    //         "merchant_uid": rsp.merchant_uid,
+                    //         "imp_uid": rsp.imp_uid,
+                    //         "apply_num": rsp.apply_num,
+                    //         "buyer_email": rsp.buyer_email,
+                    //         "product_name": orderName,
+                    //         "product_idx": productIdx,
+                    //         "schedule_id": scheduleId,
+                    //         "product_type": reqIDX,
+                    //         "content1": content1,
+                    //         "content2": content2,
+                    //         "content3": content3,
+                    //         "content4": content4,
+                    //         "product_count": productCount,
+                    //         "amount": currentPrice,
+                    //         "plusPoint": plusPoint,
+                    //         "minusPoint": minusPoint
+                    //     }),
+                    //     success: function(response_complete) {
+                    //         localStorage.clear(); // 결제 완료 시 장바구니에 담긴 물품들 삭제
+                    //         let params = new URLSearchParams();
+                    //         params.append("imp_uid", response_complete.imp_uid);
+                    //         window.location.href = '/payment/paymentSuccessful?'+ params.toString(); // 결제가 완료된 후 리디렉션할 페이지
+                    //     },
+                    //     error: function() {
+                    //         alert("서버 통신에 실패했습니다.");
+                    //     }
+                    // });
                     /* 테스트용 끝*/
 
                 }
