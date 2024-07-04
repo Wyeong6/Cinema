af7ccd0f694bd00a9dc3bffc3cd5682fcad30f81
KimKay1 <kay.kimsy@gmail.com>
2024-07-04 09:46:46 +0900

    [기능 수정] 로그아웃 상태일때 마이페이지 접근 시 메인페이지로 이동하게 수정 [리팩토링]

--- src/main/java/com/busanit/controller/MypageController.java
+++ src/main/java/com/busanit/controller/MypageController.java
@@ -50,58 +50,65 @@ public class MypageController {
 
     @GetMapping("/")
     public String mypage(@AuthenticationPrincipal Object principal, Model model) {
-        // 현재 로그인한 사용자의 이메일
-        String userEmail = memberService.currentLoggedInEmail();
+        try {
+            // 현재 로그인한 사용자의 이메일
+            String userEmail = memberService.currentLoggedInEmail();
 
-        // 사용자의 등급 확인+저장
-        long userGrade = memberService.userGrade();
+            // 사용자의 등급 확인+저장
+            long userGrade = memberService.userGrade();
 
-        // social이 true이면 SocialMemberDTO를 사용, false이면 FormMemberDTO를 사용하는 조건문
-        if(principal instanceof OAuth2MemberDTO) {
-            OAuth2MemberDTO oAuth2MemberDTO = (OAuth2MemberDTO) principal;
-            model.addAttribute("socialUser", "socialUser");
-        } else if(principal instanceof FormMemberDTO) {
-            FormMemberDTO formMemberDTO = (FormMemberDTO) principal;
-            model.addAttribute("formUser", "formUser");
+            // social이 true이면 SocialMemberDTO를 사용, false이면 FormMemberDTO를 사용하는 조건문
+            if(principal instanceof OAuth2MemberDTO oAuth2MemberDTO) {
+                model.addAttribute("socialUser", "socialUser");
+            } else if(principal instanceof FormMemberDTO formMemberDTO) {
+                model.addAttribute("formUser", "formUser");
+            }
+
+            // 사용자의 Id
+            MemberRegFormDTO memberRegFormDTO = memberService.getFormMemberInfo(userEmail);
+            model.addAttribute("memberId", memberRegFormDTO.getId());
+        } catch (NullPointerException e) {
+            return "redirect:/";
         }
 
-        // 사용자의 Id
-        MemberRegFormDTO memberRegFormDTO = memberService.getFormMemberInfo(userEmail);
-        model.addAttribute("memberId", memberRegFormDTO.getId());
         return "/layout/layout_mypage";
     }
 
     @GetMapping("/main")
     public String mypageMain(Model model, @PageableDefault(size = 5, sort = "updateDate", direction = Sort.Direction.DESC) Pageable pageable) {
-        // 현재 로그인한 사용자의 이메일
-        String userEmail = memberService.currentLoggedInEmail();
+        try {
+            // 현재 로그인한 사용자의 이메일
+            String userEmail = memberService.currentLoggedInEmail();
 
-        // 사용자의 정보 + 포인트 정보
-        MemberRegFormDTO memberRegFormDTO = memberService.getFormMemberInfo(userEmail);
-        model.addAttribute("myPageMemberInfo", memberRegFormDTO);
-        Slice<PointDTO> pointDTOList = null;
-        pointDTOList = pointService.getPointInfo(memberRegFormDTO.getId(), pageable);
-        model.addAttribute("pointInfo", pointDTOList);
+            // 사용자의 정보 + 포인트 정보
+            MemberRegFormDTO memberRegFormDTO = memberService.getFormMemberInfo(userEmail);
+            model.addAttribute("myPageMemberInfo", memberRegFormDTO);
+            Slice<PointDTO> pointDTOList = null;
+            pointDTOList = pointService.getPointInfo(memberRegFormDTO.getId(), pageable);
+            model.addAttribute("pointInfo", pointDTOList);
 
-        // 사용자의 등급 변환
-        Integer userGradeInt = memberRegFormDTO.getGrade_code();
-        String gradeString = switch (userGradeInt) {
-            case 1 -> "BLACK";
-            case 2 -> "RED";
-            case 3 -> "BLUE";
-            default -> "GREEN";
-        };
-        model.addAttribute("myPageGrade", gradeString);
+            // 사용자의 등급 변환
+            Integer userGradeInt = memberRegFormDTO.getGrade_code();
+            String gradeString = switch (userGradeInt) {
+                case 1 -> "BLACK";
+                case 2 -> "RED";
+                case 3 -> "BLUE";
+                default -> "GREEN";
+            };
+            model.addAttribute("myPageGrade", gradeString);
 
-        // 최근 예매 내역
-        Slice<PaymentDTO> moviePaymentDTOList = null;
-        moviePaymentDTOList = paymentService.getMoviePaymentInfo(memberService.findUserIdx(userEmail), pageable);
-        model.addAttribute("moviePaymentInfo", moviePaymentDTOList);
+            // 최근 예매 내역
+            Slice<PaymentDTO> moviePaymentDTOList = null;
+            moviePaymentDTOList = paymentService.getMoviePaymentInfo(memberService.findUserIdx(userEmail), pageable);
+            model.addAttribute("moviePaymentInfo", moviePaymentDTOList);
 
-        // 최근 스낵 주문 내역
-        Slice<PaymentDTO> snackPaymentDTOList = null;
-        snackPaymentDTOList = paymentService.getSnackPaymentInfo(memberService.findUserIdx(userEmail), pageable);
-        model.addAttribute("snackPaymentInfo", snackPaymentDTOList);
+            // 최근 스낵 주문 내역
+            Slice<PaymentDTO> snackPaymentDTOList = null;
+            snackPaymentDTOList = paymentService.getSnackPaymentInfo(memberService.findUserIdx(userEmail), pageable);
+            model.addAttribute("snackPaymentInfo", snackPaymentDTOList);
+        } catch (NullPointerException e) {
+            return "redirect:/";
+        }
 
         return "/mypage/mypage_main";
     }
